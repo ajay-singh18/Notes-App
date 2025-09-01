7656d7d9ed7cad9ae76576c01bf71f350e5c857b
Ajay Kumar <ajaykrsingh8709@gmail.com>
2025-09-02 01:04:29 +0530

    finished frontend

--- frontend/notes-app/src/components/Input/TagInput.jsx
+++ frontend/notes-app/src/components/Input/TagInput.jsx
@@ -25,7 +25,7 @@ const TagInput = ({tags,setTags}) => {
     <div>
         {tags.length >0 && <div className='flex items-center gap-2 flex-wrap mt-2'>
             {tags.map((tag,index) => 
-                (<span key = {index} className=''>
+                (<span key = {index} className='flex items-center gap-2 text-sm text-slate-900 bg-slate-100 px-3 py-1 rounded'>
                     # {tag}
                     <button onClick={()=>{
                         handleRemoveTag(tag)
@@ -37,9 +37,10 @@ const TagInput = ({tags,setTags}) => {
         }
         </div>}
         <div className='flex items-center gap-4 mt-3'>
-            <input type="text " className='text-sm bg-transparent border px-3 py-2 rounded outline-none' placeholder='Add tags' onChange={handleInputChange} onKeyDown={(e)=>{
-                handleKeyDown(e)
-            }} />
+            <input 
+            type="text " className='text-sm bg-transparent border px-3 py-2 rounded outline-none' placeholder='Add tags' onChange={handleInputChange} onKeyDown={handleKeyDown}
+            value={inputValue} 
+            />
             <button className='w-8 h-8 flex items-center justify-center rounded border border-blue-700 hover:bg-blue-700' onClick={addNewTag}>
                 <MdAdd className='text-2xl text-blue-700 hover:text-white'/>
             </button>
